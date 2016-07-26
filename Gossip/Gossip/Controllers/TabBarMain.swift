6e8bcc3b48cf0c7d82d41c1f08b04eccbff8fe24
zhaochenjun <holyoops@hotmail.com>
2016-07-26 18:06:44 +0800

    add tab

---
+++ Gossip/Gossip/Controllers/TabBarMain.swift
@@ -0,0 +1,13 @@
+//
+//  TabBarMain.swift
+//  Gossip
+//
+//  Created by zhaochenjun on 16/7/26.
+//  Copyright © 2016年 kazmastudio. All rights reserved.
+//
+
+import UIKit
+
+class TabBarMain: UITabBarController {
+
+}
