f69933c164b8f4ff2221d12cb8881cbdd0194d19
zhaochenjun <holyoops@hotmail.com>
2016-07-26 18:38:05 +0800

    change tab

--- Gossip/Gossip/Controllers/TabBarMain.swift
+++ Gossip/Gossip/Controllers/TabBarMain.swift
@@ -9,5 +9,33 @@
 import UIKit
 
 class TabBarMain: UITabBarController {
+    
+    override func viewWillAppear(animated: Bool) {
+        
+        for (var i = 0; i < self.tabBar.items?.count; i += 1){
+            
+            let imageName:String
+            
+            switch(i){
+            case 0:
+                imageName = "news"
+                break
+            case 1:
+                imageName = "xinyuan_c"
+                break
+            case 2:
+                imageName = "wode_c"
+                break
+            default:
+                imageName = "shouye_c"
+            }
+            
+            let unselectedImage = UIImage.init(named: imageName)
+            self.tabBar.items![i].image = unselectedImage?.imageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal)
+            self.tabBar.items![i].imageInsets = UIEdgeInsetsMake(10, 0, -10, 0)
 
+        }
+        
+    }
+    
 }
